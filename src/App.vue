6b4079d19151f7a6f7c08c57acc61ed05a8b8905
dowellkin <dowellkin@gmail.com>
2020-10-31 01:16:17 +0300

    second day

--- src/App.vue
+++ src/App.vue
@@ -4,36 +4,86 @@
 		<a-row class="header" type="flex" justify="space-around" align="middle" style="margin-bottom: 5px">
 			<a-col>
 				<a-icon class="icon" type="plus" @click="openmodal('add')"/>
-				<a-modal :visible="modal.add.visible" @cancel="closemodal('add')">
-					<span slot="percent" slot-scope="text">{{text}}%</span>
-					<span slot="price" slot-scope="text">{{text}}р.</span>
+				<a-modal :visible="modal.add.visible" @cancel="closemodal('add')" @ok='addSubmit()'>
+					<div slot="footer">
+						<a-button type="danger" v-if="modal.add.currentid != -1" @click="deleteLine()">Удалить</a-button>
+						<a-button @click="closemodal('add')">Отмена</a-button>
+						<a-button @click='addSubmit()' type="primary">Ок</a-button>
+					</div>
+					<h2>Добавить позицию</h2>
+					<p>
+						Название пива
+						<a-input v-model="modal.add.info.name"></a-input>
+					</p>
+					<p>
+						Страна производства
+						<a-input v-model="modal.add.info.country"></a-input>
+					</p>
+					<p>
+						Крепость
+						<a-input v-model="modal.add.info.percent"></a-input>
+					</p>
+					<p>
+						Светлое-темное
+						<a-row type="flex">
+							<a-col flex=1>
+								<a-select class="modalSelect" v-model="modal.add.info.brightnessSwitch">
+									<a-select-option v-for="bright in getBrightness" :key="bright" :value="bright">
+										{{bright}}
+									</a-select-option>
+								</a-select>
+							</a-col>
+							<a-col flex=3 v-if="modal.add.info.brightnessSwitch == 'ввести'">
+								<a-input v-model="modal.add.info.brightness"></a-input>
+							</a-col>
+						</a-row>
+					</p>
+					<p>
+						Характеристика
+						<a-row type="flex">
+							<a-col flex=1>
+								<a-select class="modalSelect" v-model="modal.add.info.typeSwitch">
+									<a-select-option v-for="type in getType" :key="type" :value="type">
+										{{type}}
+									</a-select-option>
+								</a-select>
+							</a-col>
+							<a-col flex=3 v-if="modal.add.info.typeSwitch == 'ввести'">
+								<a-input v-model="modal.add.info.type"></a-input>
+							</a-col>
+						</a-row>
+					</p>
+					<p>
+						Стоимость
+						<a-input v-model="modal.add.info.price"></a-input>
+					</p>
 				</a-modal>
 			</a-col>
 			<a-col>
 				<a-icon class="icon" type="setting" @click="openmodal('settings')"/>
 				<a-modal :visible="modal.settings.visible" @cancel="closemodal('settings')">
 					<p>
-						test
+						Количество строк на экране
 						<a-input v-model="onScreen" type="number" min=1></a-input>
 					</p>
 				</a-modal>
 			</a-col>
-			<a-col>
-				<a-pagination v-model="page" :total="beerData.length" :pageSize="onScreen" @change="onPageChange"/>
+			<a-col v-if="getBeer.length > onScreen">
+				<a-pagination v-model="page" :total="getBeer.length" :pageSize="onScreen" @change="onPageChange"/>
 			</a-col>
 		</a-row>
 
 		<a-config-provider>
 			<template #renderEmpty>
-        <div style="text-align: center">
+        <div style="text-align: center; cursor: pointer" @click="openmodal('add')">
           <a-icon type="smile" style="font-size: 20px" />
-          <p>Data Not Found</p>
+          <p>Добавтье продукт с помощью "+" в левом верхнем углу экрана</p>
         </div>
       </template>
 			<a-table
 				class="mainTable"
 				:columns="columns"
-				:data-source="beerData"
+				:data-source="getBeer"
 				size="large"
 				:showHeader="false"
 				style="height: 100%"
@@ -41,17 +91,23 @@
 				tableLayout="auto"
 				rowKey="id"
 				@change="onPageChange"
-			/>
+			>
+				<span slot="name" slot-scope="text, line" @click="out(line)">{{text}}</span>
+				<span slot="percent" slot-scope="text">{{text}}%</span>
+				<span slot="price" slot-scope="text">{{text}}р.</span>
+			</a-table>
 		</a-config-provider>
   </div>
 </template>
 
 <script>
+import {mapGetters, mapMutations, mapActions} from 'vuex';
 
 const columns = [
 	{
 		title: 'Name',
-		dataIndex: 'name'
+		dataIndex: 'name',
+		scopedSlots: { customRender: 'name' }
 	},
 	{
 		title: 'percent',
@@ -77,32 +133,28 @@ const columns = [
 	},
 ]
 
-const beerData = []
-
-for(let i = 0; i < 50; i++){
-	beerData.push({
-		id: i,
-		name: "Пиво мощнейшее #" + i ,
-		percent: "2.4",
-		country: "чх",
-		brightness: "светлое",
-		type: "нефильтрованное",
-		price: "2.2"
-	})
-}
-
 export default {
 	name: 'App',
 	data(){
 		return {
 			columns,
-			beerData,
 			page: 1,
 			onScreen: 14,
 			interval: 0,
 			modal: {
 				add: {
-					visible: false
+					visible: false,
+					info: {
+						name: "",
+						percent: "",
+						country: "",
+						brightness: "",
+						brightnessSwitch: "пусто",
+						type: "",
+						typeSwitch: "пусто",
+						price: "",
+					},
+					currentid: -1,
 				},
 				settings: {
 					visible: false
@@ -114,14 +166,18 @@ export default {
 	},
 	computed:{
 		pag(){
-			if(this.beerData.length > this.onScreen){
+			if(this.getBeer.length > this.onScreen){
 				return { pageSize: this.onScreen, position: 'top', current: this.page }
 			} else {
 				return false
 			}
-		}
+		},
+		...mapGetters(['getBeer', 'getBrightness', 'getType'])
 	},
   methods: {
+		...mapMutations(['addBeer', 'removeById']),
+		...mapGetters(['getId']),
+		...mapActions(['updateBeerField']),
     onPageChange(pagination) {
 			if(pagination.current == undefined){
 				this.page = pagination
@@ -135,7 +191,7 @@ export default {
 			}, 25000);
 		},
 		nextPage(){
-			const pages = Math.ceil(this.beerData.length / this.onScreen)
+			const pages = Math.ceil(this.getBeer.length / this.onScreen)
 			if(this.page < pages){
 				this.page += 1;
 			} else {
@@ -147,7 +203,90 @@ export default {
 		},
 		closemodal(name){
 			this.modal[name].visible = false
+			this.clearAddFields()
+			if(name == 'add') this.modal.add.currentid = -1
+		},
+		out(line){
+			const id = line.id;
+			this.modal.add.currentid = id;
+			
+			this.modal.add.info = {
+				name: line.name,
+				percent: line.percent,
+				country: line.country,
+				brightness: line.brightness,
+				brightnessSwitch: "ввести",
+				type: line.type,
+				typeSwitch: "ввести",
+				price: line.price,
+			}
 
+			this.openmodal('add');
+		},
+		clearAddFields(){
+			this.modal.add.info = {
+				name: "",
+				percent: "",
+				country: "",
+				brightness: "",
+				brightnessSwitch: "пусто",
+				type: "",
+				typeSwitch: "пусто",
+				price: "",
+			}
+			this.modal.add.currentid = -1
+		},
+		addSubmit(){
+			const adMod = this.modal.add
+			const inf = adMod.info
+			if(inf.name == ''){
+				this.$message.error('Добавьте название');
+				return;
+			}
+			
+			let result = {
+				id: this.getId(),
+				name: inf.name,
+				percent: inf.percent,
+				country: inf.country,
+				price: inf.price
+			}
+			if(adMod.currentid != -1){
+				result.id = adMod.currentid;
+			}
+			const brightnessSwitch = inf.brightnessSwitch;
+			if(brightnessSwitch == "ввести"){
+				result.brightness = inf.brightness
+			} else if(brightnessSwitch == 'пусто') {
+				result.brightness = ''
+			} else {
+				result.brightness = inf.brightnessSwitch
+			}
+			const typeSwitch = inf.typeSwitch;
+			if(typeSwitch == "ввести"){
+				result.type = inf.type
+			} else if(typeSwitch == 'пусто') {
+				result.type = ''
+			} else {
+				result.type = inf.typeSwitch
+			}
+
+			if(this.modal.add.currentid == -1){
+				this.addBeer(result)
+				this.$message.success('Позиция добавлена успешно');
+			} else {
+				this.updateBeerField(result);
+				this.$message.success('Позиция обновлена успешно');
+			}
+
+			this.clearAddFields();
+			this.modal.add.visible = false;
+		},
+		deleteLine(){
+			this.removeById(this.modal.add.currentid);
+			this.$message.success('Позиция удалена успешно');
+			this.clearAddFields();
+			this.modal.add.visible = false;
 		}
 	},
 	mounted(){
@@ -180,7 +319,8 @@ body{
 }
 
 .mainTable .ant-table-placeholder{
-	/* background: transparent; */
+	background: transparent;
+	color: rgb(255 255 255 / 33%);
 	border: none
 }
 
@@ -195,6 +335,7 @@ body{
 	font-size: 36px;
 	color: #fff;
 	cursor: pointer;
+	user-select: none;
 }
 .header{
 	color: #fff;
@@ -226,5 +367,9 @@ body{
 
 .ant-table-pagination{
 	display: none;
+}
+
+.modalSelect{
+	width: 100%;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,51 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+		beer: [],
+		brightness: ['пусто', 'светлое', 'темное', 'ввести'],
+		type: ['пусто', 'паст.фил.', 'паст.нефил.', 'непаст.фил.', 'непаст.нефил.', 'ввести']
   },
   mutations: {
+		addBeer(state, data){
+			state.beer.push(data);
+		},
+		setBeer(state, value){
+			state.beer = value;
+		},
+		removeById(state, value){
+			console.log(value);
+			let index = state.beer.findIndex(el => el.id == value);
+			if(index != -1)
+				state.beer.splice(index,1)
+			console.log(index);
+		},
+		setSomeIndexBeer(state, value){
+			const item = state.beer.find(el => el.id == value.value.id)
+			Object.assign(item, value.value)
+		}
   },
   actions: {
-  },
+		updateBeerField(ctx, value){
+			const beer = ctx.getters.getBeer;
+			let index = beer.findIndex(el => el.id == value.id);
+			ctx.commit('setSomeIndexBeer', {index, value});
+		}
+	},
+	getters: {
+		getBeer(state){
+			return state.beer
+		},
+		getBrightness(state){
+			return state.brightness
+		},
+		getType(state){
+			return state.type
+		},
+		getId(state){
+			if(state.beer.length == 0) return 0 
+			return Math.max(...state.beer.map(el=>el.id))+1
+		}
+	},
   modules: {
   }
 })
